5ee1e016aa55c72106693f378dd90805e073d9ea
Pooja Kelkar <poojakelkar53@gmail.com>
2021-09-11 18:37:43 +0530

    translation done

--- app.js
+++ app.js
@@ -2,8 +2,30 @@ var btnTranslator = document.querySelector("#btn-translator");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler() {
-    outputDiv.innerText = "translation Done" + textInput.value;
+var serverURL="https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL +"?" +"text=" +text
 }
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Sorry, something went wrong with server try after some time!")
+}
+
+function clickHandler() {
+    //outputDiv.innerText = "translation Done" + textInput.value;
+    var inputText= textInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response=>response.json())
+        .then(json=>{
+            var translatedText=json.contents.translated
+            outputDiv.innerText=translatedText
+        })
+        .catch(errorHandler)
+    
+}
+
 
 btnTranslator.addEventListener("click", clickHandler);
